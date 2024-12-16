1d630d72fc0657a0966ec125cf0a251d1024832f
Jyotirmay Pande <jyotirmay.pande@newgensoft.com>
2024-12-16 20:30:58 +0530

    updated code for multithreading

--- src/LLD/SingletonDesign/SingletonCaller.java
+++ src/LLD/SingletonDesign/SingletonCaller.java
@@ -4,12 +4,8 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Objects;
 
-public class BuilderCaller {
+public class SingletonCaller {
     public static void main(String[] args) throws CloneNotSupportedException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-//        User.UserBuilder userBuilder = new User.UserBuilder();
-//        // new User.UserBuilder(); Creating object of UserBuilder() as without creating object user class as UserBuilder is a static class.
-//        userBuilder.setName("Jay").setEmail("dhfds@gma.com");
-//        User user = userBuilder.build();
 
         /*
         How to break singleton pattern using clone()
---
+++ src/afternewgenprac/MultithreadingEvenOdd.java
@@ -0,0 +1,64 @@
+package afternewgenprac;
+
+public class MultithreadingEvenOdd {
+    static int counter;
+    public static Object LOCK = new Object();
+    public static boolean guard = true;
+
+    public static void even() throws InterruptedException {
+        synchronized (LOCK) {
+            while (counter <= 100) {
+                if (counter % 2 == 0) {
+                    System.out.println("Even Number is :" + counter);
+                    counter++;
+                }
+                if(!guard){
+                    LOCK.notify();
+                    guard=true;
+                    LOCK.wait();
+                }
+            }
+            LOCK.notify();
+        }
+    }
+
+    public static void odd() throws InterruptedException {
+        synchronized (LOCK) {
+            while (counter <= 100) {
+                if (counter % 2 != 0) {
+                    System.out.println("Odd Number is :" + counter);
+                    counter++;
+                }
+                if(guard){
+                    guard=false;
+                    LOCK.notify();
+                    LOCK.wait();
+                }
+            }
+            LOCK.notify();
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+      Thread even = new Thread(()->{
+          try {
+              even();
+          } catch (InterruptedException e) {
+              throw new RuntimeException(e);
+          }
+      });
+
+      Thread odd = new Thread(()->{
+          try {
+              odd();
+          } catch (InterruptedException e) {
+              throw new RuntimeException(e);
+          }
+      });
+      odd.start();
+      even.start();
+      odd.join();
+      even.join();
+    }
+}
